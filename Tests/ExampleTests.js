4cfe577776e076957afd339c2e1131410653288a
losuna <losuna@bofifederalbank.com>
2022-07-21 21:02:57 -0500

    Step 1 - Declare a Page Model Class

---
+++ Tests/ExampleTests.js
@@ -0,0 +1,11 @@
+import page from '../PageObjects/ExamplePage/mainPage';
+
+fixture `My fixture`
+    .page `https://devexpress.github.io/testcafe/example/`;
+
+test('Text typing basics', async t => {
+    await t
+        .typeText(page.nameInput, 'Peter')
+        .typeText(page.nameInput, 'Paker', { replace: true })
+        .expect(page.nameInput.value).eql('Parker');
+});
--- page-model.js
+++
@@ -1,9 +0,0 @@
-import { Selector } from 'testcafe';
-
-class Page {
-    constructor () {
-        this.nameInput = Selector('#developer-name');
-    }
-}
-
-export default new Page();
